bbb922fd136670d9b4b1e1856d0ed7572f80d6a6
Chia Sheng <chiasheng@feedme.cc>
2023-06-09 14:15:39 +0800

    switch to hook version

--- dart/flutter_application_1/lib/main.dart
+++ dart/flutter_application_1/lib/main.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:core';
 import 'package:collection/collection.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_application_1/models/order.dart';
 import 'package:flutter_application_1/models/bot.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 
-final orderProvider = StateProvider<List<Order>>((_) {
+final orderProvider = StateProvider<List<Order?>>((_) {
+  return [];
+});
+
+final botProvider = StateProvider<List<Bot?>>((_) {
   return [];
 });
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
+
+class OrderNotifier extends ChangeNotifier {}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -40,7 +48,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends HookConsumerWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -55,110 +63,33 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    var _isPressed = useState(false);
+    var _orderUniqueId = useState(0);
+    var _botUniqueId = useState(0);
 
-class MyListView extends StatefulWidget {
-  const MyListView(this.title, this.list);
+    var orders = useState([]);
+    var bots = useState([]);
 
-  final List list;
-  final String title;
-  
-  @override
-  Widget build(BuildContext context) {
+    void processOrder(Bot? bot) {
+      // check if the bot is null
+      if (bot == null) return;
 
-    return Flexible(
-      child: Column(
-        children: <Widget>[
-          Text(title),
-          const Divider(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height - 100,
-            child: ListView.builder(
-              controller: ScrollController(), // Define seperate scroll controller for each listing 
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                final item = list[index];
-                return ListTile(
-                  title: Text("${item is Bot ? 'Bot(s)' : 'Order(s)'} ${item.uniqueId}"),
-                  subtitle: item is Order
-                    ? Text(item.vip ? 'VIP' : 'Normal')
-                    : Text("${item.remaining}"),
-                );
-              })
-          )
-        ],
-      ),
-    );
-  }
+      // set the initial remaining value
+      bot.remaining = 0;
 
-  @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _orderUniqueId = 0;
-  int _botUniqueId = 0;
-  bool _isPressed = false;
-  List orders = [];
-  List bots = [];
-
-  void _addOrder({bool vip = false}) {
-    setState(() {
-      if(vip) {
-        orders.insert(0, Order(_orderUniqueId, vip));
-      } else {
-        orders.add(Order(_orderUniqueId, vip));
-      }
-      _orderUniqueId++;
-      processOrder(bots.firstWhere((n) => n.order == null, orElse: () => null));
-    });
-  }
-
-  void _addBot() {
-    setState(() {
-      final bot = Bot(_botUniqueId);
-      bots.add(bot);
-      _botUniqueId++;
-      processOrder(bot);
-    });
-  }
-
-  void _removeBot() {
-    setState(() {
-      Bot bot = bots.last;
-      Order? order = bot.order;
+      // cancel the previous timer if any
       bot.timer?.cancel();
-      setState(() {
-        order?.bot = null;
-      });
-      bots.remove(bot);
-      processOrder(bots.firstWhere((n) => n.order == null, orElse: () => null));
-    });
-  }
 
-  void processOrder(Bot? bot) {
-    // check if the bot is null
-    if (bot == null) return;
+      Order? order = orders.value.firstWhere(
+          (n) => n.bot == null && n.completedAt == null,
+          orElse: () => null);
 
-    // set the initial remaining value
-    bot.remaining = 0;
-
-    // cancel the previous timer if any
-    bot.timer?.cancel();
-
-    Order? order = orders.firstWhere((n) => n.bot == null && n.completedAt == null, orElse: () => null);
-
-    if(order != null) {
-      setState(() {
+      if (order != null) {
         bot.remaining = 10;
         bot.order = order;
         order.bot = bot;
-      });
-      bot.timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-        setState(() {
+        bot.timer = Timer.periodic(const Duration(seconds: 1), (timer) {
           bot.remaining = bot.remaining! - 1;
           if (bot.remaining! <= 0) {
             timer.cancel();
@@ -166,126 +97,140 @@ class _MyHomePageState extends State<MyHomePage> {
             bot.order = null;
             processOrder(bot);
           }
+
+          bots.value = [...bots.value];
         });
-      });
+      }
     }
 
-  }
+    void _addOrder({bool vip = false}) {
+      if (vip) {
+        orders.value = [Order(_orderUniqueId.value, vip), ...orders.value];
+      } else {
+        orders.value = [...orders.value, Order(_orderUniqueId.value, vip)];
+      }
+      _orderUniqueId.value++;
+      processOrder(
+          bots.value.firstWhere((n) => n.order == null, orElse: () => null));
+    }
 
-  ListTile _listTile(item) {
-    return item is Bot
-    ? ListTile(
-      title: Text('Bot(s) #${item.uniqueId}'),
-      subtitle: item.order != null ? Row(
-        children: [
-          Text('Processing Order #${item.order?.uniqueId}'),
-          Text("(${item.remaining}s left)"),
-        ]
-      ) : null
-    )
-    : ListTile(
-      title: Text('Order(s) #${item.uniqueId}'),
-      subtitle: Text(item.vip ? 'VIP' : 'Normal'),
-    );
-  }
+    void _addBot() {
+      final bot = Bot(_botUniqueId.value);
+      bots.value = [...bots.value, bot];
+      _botUniqueId.value++;
+      processOrder(bot);
+    }
 
-  Widget _listView(title, list) {
-    return Flexible(
-      child: Column(
-        children: <Widget>[
-          const SizedBox(height: 10),
-          Text(title),
-          const SizedBox(height: 10),
-          const Divider(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height - 120,
-            child: ListView.builder(
-              controller: ScrollController(), // Define seperate scroll controller for each listing 
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                return _listTile(list[index]);
-              })
-          )
-        ],
-      ),
-    );
-  }
+    void _removeBot() {
+      Bot? bot = bots.value.isNotEmpty ? bots.value.last : null;
 
+      if (bot == null) {
+        return;
+      }
 
+      Order? order = bot.order;
+      bot.timer?.cancel();
+      order?.bot = null;
+      bots.value = [...bots.value, bot]..remove(bot);
+      processOrder(
+          bots.value.firstWhere((n) => n.order == null, orElse: () => null));
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    ListTile _listTile(item) {
+      return item is Bot
+          ? ListTile(
+              title: Text('Bot(s) #${item.uniqueId}'),
+              subtitle: item.order != null
+                  ? Row(children: [
+                      Text('Processing Order #${item.order?.uniqueId}'),
+                      Text("(${item.remaining}s left)"),
+                    ])
+                  : null)
+          : ListTile(
+              title: Text('Order(s) #${item.uniqueId}'),
+              subtitle: Text(item.vip ? 'VIP' : 'Normal'),
+            );
+    }
+
+    Widget _listView(title, list) {
+      return Flexible(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 10),
+            Text(title),
+            const SizedBox(height: 10),
+            const Divider(),
+            SizedBox(
+                height: MediaQuery.of(context).size.height - 120,
+                child: ListView.builder(
+                    controller:
+                        ScrollController(), // Define seperate scroll controller for each listing
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      return _listTile(list[index]);
+                    }))
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Row(children: [
-        _listView("Bot(s)", bots),
-        _listView("Pending Order(s)", orders.where((i) => i.completedAt == null).toList()),
-        _listView("Completed Order(s)", orders.where((i) => i.completedAt != null).toList()),
-      ])
-      ,
-      floatingActionButtonLocation: ExpandableFab.location,
-      floatingActionButton: ExpandableFab(children: [
-        FloatingActionButton(
-          onPressed: () {
-            if(!_isPressed) {
-              setState(() {
-                _isPressed = true; // set the button state to true
-              });
-              _addBot();
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Row(children: [
+          _listView("Bot(s)", bots.value),
+          _listView("Pending Order(s)",
+              orders.value.where((i) => i?.completedAt == null).toList()),
+          _listView("Completed Order(s)",
+              orders.value.where((i) => i?.completedAt != null).toList()),
+        ]),
+        floatingActionButtonLocation: ExpandableFab.location,
+        floatingActionButton: ExpandableFab(
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                if (!_isPressed.value) {
+                  _isPressed.value = true;
+                  _addBot();
 
-              Future.delayed(const Duration(milliseconds: 500), () {
-                setState(() {
-                  _isPressed = false; // set the button state to false when done
-                });
-              });
-            }
-        }, // use arrow function,
-          tooltip: 'New Bot',
-          child: const Icon(Icons.group_add),
-        ),
-        FloatingActionButton(
-          onPressed: () {
-            if(!_isPressed) {
-              setState(() {
-                _isPressed = true; // set the button state to true
-              });
-              _removeBot();
+                  Future.delayed(const Duration(milliseconds: 500), () {
+                    _isPressed.value = false;
+                  });
+                }
+              }, // use arrow function,
+              tooltip: 'New Bot',
+              child: const Icon(Icons.group_add),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                if (!_isPressed.value) {
+                  _isPressed.value = true;
+                  _removeBot();
 
-              Future.delayed(const Duration(milliseconds: 500), () {
-                setState(() {
-                  _isPressed = false; // set the button state to false when done
-                });
-              });
-            }
-        }, // use arrow function,
-          tooltip: 'Remove Bot',
-          backgroundColor: Colors.red,
-          child: const Icon(Icons.group_remove),
-        ),
-        FloatingActionButton(
-          onPressed: _addOrder,
-          tooltip: 'New Order',
-          child: const Icon(Icons.add),
-        ),
-        FloatingActionButton(
-          backgroundColor: Colors.orange,
-          onPressed: () => _addOrder(vip: true),
-          tooltip: 'New VIP Order',
-          child: const Icon(Icons.add),
-        ),
-      ],)
-    );
+                  Future.delayed(const Duration(milliseconds: 500), () {
+                    _isPressed.value = false;
+                  });
+                }
+              }, // use arrow function,
+              tooltip: 'Remove Bot',
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.group_remove),
+            ),
+            FloatingActionButton(
+              onPressed: _addOrder,
+              tooltip: 'New Order',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              backgroundColor: Colors.orange,
+              onPressed: () => _addOrder(vip: true),
+              tooltip: 'New VIP Order',
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ));
   }
 }
